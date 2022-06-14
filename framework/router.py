3069cc5e17210a9e283600569bb1e637955d6cd1
Ramon Almeida <ramongoncalves76@gmail.com>
2022-06-13 22:47:11 -0300

    Methods renamed

--- framework/router.py
+++ framework/router.py
@@ -2,16 +2,16 @@ from typing import Callable, Dict, List, List
 from framework.error import BadConstructionError, FunctionNotImplementedError
 
 from protocol.frame import Frame, FrameBody, FrameHeader
-from protocol.protocoltypes import HeaderLabelType, MethodType
+from protocol.protocoltypes import HeaderLabelType, MethodType, SCodeType
 
 
 class Router:
 
-    def __init__(self, id: int, label: str) -> None:
+    def __init__(self, label: str, id: int = 1) -> None:
 
-        self._id = id
-        self._label = label
-        self._methods = {}
+        self._id: int = id
+        self._label: str = label
+        self._methods: Dict[int, Callable] = {}
 
     def get_id(self) -> int:
         return self._id
@@ -19,18 +19,20 @@ class Router:
     def get_label(self) -> str:
         return self._label
 
-    def get_routine(self, mt_id: int):
+    def get_methods(self) -> Dict[int, Callable]:
+        return self._methods
 
+    def get_routine(self, mt_id: int):
         return self._methods.get(mt_id)
 
     def auth(self, func: Callable[[Frame], Frame]):
         self._methods[MethodType.AUTH.value] = func
 
-    def create(self, func: Callable[[Frame], Frame]):
-        self._methods[MethodType.CREATE.value] = func
+    def push(self, func: Callable[[Frame], Frame]):
+        self._methods[MethodType.PUSH.value] = func
 
-    def list(self, func: Callable[[Frame], Frame]):
-        self._methods[MethodType.LIST.value] = func
+    def pull(self, func: Callable[[Frame], Frame]):
+        self._methods[MethodType.PULL.value] = func
 
     def join(self, func: Callable[[Frame], Frame]):
         self._methods[MethodType.JOIN.value] = func
@@ -38,43 +40,16 @@ class Router:
 
 class RouterManager:
 
-    def __init__(self) -> None:
+    def __init__(self, router_controller: Router = None) -> None:
 
         self._routes: Dict[int, Router] = {}
+
+        if router_controller is not None:
+            self._routes[1] = router_controller
 
     def get_routes(self) -> Dict[int, Router]:
 
         return self._routes
-
-    def solver(self, frame: Frame) -> Frame:
-
-        hearder = frame.get_header()
-
-        rs = hearder.get(HeaderLabelType.RS.value)
-
-        if rs is None:
-            raise BadConstructionError('Recurso não informado')
-
-        if rs == 1:
-            return Frame(FrameHeader({'tt': 'kp'}), FrameBody(self._get_rss()))
-
-        mt = hearder.get(HeaderLabelType.METHOD.value)
-
-        if mt is None:
-            raise BadConstructionError('Método não informado')
-
-        router = self._routes[rs]
-
-        if router is None or MethodType(mt) is None:
-            raise BadConstructionError('Recurso não encontrado')
-
-        routine = router.get_routine(mt)
-
-        if routine is None:
-            raise FunctionNotImplementedError(
-                'Recurso não localizado/implementado')
-
-        return routine(frame)
 
     def _get_rss(self) -> List[Dict[str, any]]:
 
@@ -93,3 +68,36 @@ class RouterManager:
             raise Exception('Recurso com esse id já existe')
 
         self._routes[router.get_id()] = router
+
+    def solver(self, frame: Frame) -> Frame:
+
+        hearder = frame.get_header()
+
+        rs = hearder.get(HeaderLabelType.RS.value)
+
+        if rs is None:
+            raise BadConstructionError('Recurso não informado')
+
+        if rs == 1 and self._routes.get(1) is None:
+            return Frame(
+                FrameHeader({'sc': SCodeType.SUCCESS.value}),
+                FrameBody(self._get_rss())
+            )
+
+        mt = hearder.get(HeaderLabelType.METHOD.value)
+
+        if mt is None:
+            raise BadConstructionError('Método não informado')
+
+        router = self._routes[rs]
+
+        if router is None or MethodType(mt) is None:
+            raise BadConstructionError('Recurso não encontrado')
+
+        routine = router.get_routine(mt)
+
+        if routine is None:
+            raise FunctionNotImplementedError(
+                'Recurso não localizado/implementado')
+
+        return routine(frame)
