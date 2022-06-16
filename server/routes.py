4ab67c033d69ea176ca8ef6346dac756470e8216
Ramon Almeida <ramongoncalves76@gmail.com>
2022-06-16 16:38:58 -0300

    Chat usecases implemented

--- server/routes.py
+++ server/routes.py
@@ -2,17 +2,24 @@ from typing import List
 from framework.router import Router, RouterManager
 from protocol.frame import Frame, FrameBody, FrameHeader
 from protocol.protocoltypes import HeaderLabelType, SCodeType
-from server.usecases import ChatUseCases, repo_use_cases
+from server.usecases import AppUseCases
+
+use_case = AppUseCases()
 
 
 def router_controller(routes_all: List[Router]) -> Router:
+
     router = Router('controller')
+    routes = [router] + routes_all
 
     def auth(frame_req=Frame) -> Frame:
 
+        pub_key, _ = use_case.get_rsa()
+
         header_res = FrameHeader()
         header_res.set_status_code(SCodeType.SUCCESS.value)
-        header_res.set_apk('public key')
+        header_res.set_apk(str(pub_key.n) + ' ' + str(pub_key.e))
+        header_res.set_rsa(2048)
 
         return Frame(header_res, FrameBody())
 
@@ -24,7 +31,7 @@ def router_controller(routes_all: List[Router]) -> Router:
         return Frame(header_res, FrameBody([
             {'id': r.get_id(), 'label': r.get_label(),
              'methods': list(r.get_methods().keys())}
-            for r in routes_all
+            for r in routes
         ]))
 
     router.auth(auth)
@@ -38,9 +45,8 @@ def router_user() -> Router:
 
     def auth(frame: Frame) -> Frame:
 
-        chat: ChatUseCases = repo_use_cases['chat']
-        username = frame.get_header().get_data()[HeaderLabelType.KEY.value]
-        token = chat.auth_user(username)
+        username = frame.get_header()[HeaderLabelType.KEY.value]
+        token = use_case.new_user(username)
 
         header_res = FrameHeader()
         header_res.set_status_code(SCodeType.SUCCESS.value)
@@ -56,27 +62,85 @@ def router_user() -> Router:
 def router_chat() -> Router:
     router = Router('chat', 11)
 
-    def create(frame: Frame) -> Frame:
+    def push(frame: Frame) -> Frame:
 
-        name_room = frame.get_body().get_data()['room']
-        chat: ChatUseCases = repo_use_cases['chat']
-        id = chat.create_room(name_room)
+        uuid_user = frame.get_header()[HeaderLabelType.KEY.value]
+        uuid_chat = use_case.new_chat(uuid_user)
 
         header_res = FrameHeader()
         header_res.set_status_code(SCodeType.SUCCESS.value)
-        header_res.set_room_key(id)
+        header_res.set_room_key(uuid_chat)
 
         return Frame(header_res, FrameBody())
 
     def pull(frame: Frame) -> Frame:
 
-        chat: ChatUseCases = repo_use_cases['chat']
         header_res = FrameHeader()
         header_res.set_status_code(SCodeType.SUCCESS.value)
 
-        return Frame(header_res, FrameBody(chat.list_room()))
+        chats = [
+            {'uuid': i.get_uuid(), 'owner': i.get_owner().get_name(),
+             'users': [{'username': u.get_name()}for u in i.get_users()]
+             } for i in use_case.get_chats()
+        ]
 
-    router.push(create)
+        return Frame(header_res, FrameBody(chats))
+
+    def join(frame: Frame) -> Frame:
+
+        uuid_user = frame.get_header()[HeaderLabelType.KEY.value]
+        uuid_chat = frame.get_header()[HeaderLabelType.ROOMKEY.value]
+        use_case.join_user_chat(uuid_user, uuid_chat)
+
+        header_res = FrameHeader()
+        header_res.set_status_code(SCodeType.SUCCESS.value)
+
+        return Frame(header_res, FrameBody())
+
+    router.push(push)
+    router.pull(pull)
+    router.join(join)
+
+    return router
+
+
+def router_message() -> Router:
+    router = Router('message', 12)
+
+    def push(frame: Frame) -> Frame:
+
+        uuid_user = frame.get_header()[HeaderLabelType.KEY.value]
+        uuid_chat = frame.get_header()[HeaderLabelType.ROOMKEY.value]
+        text = frame.get_body()['text']
+
+        header_res = FrameHeader()
+        header_res.set_status_code(SCodeType.SUCCESS.value)
+
+        use_case.send_message_chat(uuid_user, uuid_chat, text)
+
+        return Frame(header_res, FrameBody())
+
+    def pull(frame: Frame) -> Frame:
+
+        uuid_user = frame.get_header()[HeaderLabelType.KEY.value]
+        uuid_chat = frame.get_header()[HeaderLabelType.ROOMKEY.value]
+        last_update = frame.get_header()[HeaderLabelType.LASTUPDATE.value]
+
+        header_res = FrameHeader()
+        header_res.set_status_code(SCodeType.SUCCESS.value)
+
+        messages = [
+            {'text': m.get_content(), 'moment': m.get_moment(),
+             'username': m.get_user().get_name()}
+            for m in use_case.get_messages_chat(uuid_user, uuid_chat, last_update)
+        ]
+
+        return Frame(
+            header_res,
+            FrameBody(messages)
+        )
+
+    router.push(push)
     router.pull(pull)
 
     return router
@@ -85,6 +149,7 @@ def router_chat() -> Router:
 routes_all = [
     router_user(),
     router_chat(),
+    router_message(),
 ]
 
 router_manager = RouterManager(router_controller(routes_all))
--- server/usecases.py
+++ server/usecases.py
@@ -1,6 +1,7 @@
+from time import time_ns
 from typing import List, Tuple
 from uuid import uuid4
-from server.entities import AppEntity, ChatEntity, UseRegEntity, UserEntity
+from server.entities import AppEntity, ChatEntity, MessageEntity, UseRegEntity, UserEntity
 from server.errors import NotFoundError
 from rsa import PublicKey, PrivateKey
 
@@ -28,11 +29,11 @@ class AppUseCases:
         user_reg = UseRegEntity(uuid, user)
 
         app.add_user(user)
-        app.add_users_reg(user_reg)
+        app.add_users_reg(uuid, user_reg)
 
         return uuid
 
-    def new_chat(self, uuid_user: str, name_char: str) -> str:
+    def new_chat(self, uuid_user: str) -> str:
 
         user_reg = app.get_users_reg_by_uuid(uuid_user)
 
@@ -62,3 +63,39 @@ class AppUseCases:
     def get_chats(self) -> List[ChatEntity]:
 
         return app.get_chats()
+
+    def send_message_chat(self, uuid_user: str, uuid_chat: str, text: str) -> None:
+
+        user_reg = app.get_users_reg_by_uuid(uuid_user)
+
+        if user_reg is None:
+            raise Exception('Usuário não existe')
+
+        chat = app.get_chats_by_uuid(uuid_chat)
+
+        if chat is None:
+            raise Exception('Chat não existe')
+
+        if chat.get_user_by_uuid(uuid_user) is None:
+            raise Exception('Usuário não faz parte de chat')
+
+        message = MessageEntity(text, user_reg.get_user(), time_ns())
+
+        chat.add_message(message)
+
+    def get_messages_chat(self, uuid_user: str, uuid_chat: str, from_moment: int = 0) -> List[MessageEntity]:
+
+        user_reg = app.get_users_reg_by_uuid(uuid_user)
+
+        if user_reg is None:
+            raise Exception('Usuário não existe')
+
+        chat = app.get_chats_by_uuid(uuid_chat)
+
+        if chat is None:
+            raise Exception('Chat não existe')
+
+        if chat.get_user_by_uuid(uuid_user) is None:
+            raise Exception('Usuário não faz parte de chat')
+
+        return chat.get_messages(from_moment)
